e83316208a5871e830bb4abd7d7ff323ca904b81
Lukas Meier <lumeier@student.ethz.ch>
2016-07-29 21:08:08 +0200

    added cv_bridge and OpenCV

--- include/duo_avoidance.hpp
+++ include/duo_avoidance.hpp
@@ -8,13 +8,19 @@
 #define __DUO_AVOIDANCE_HPP__
 
 #include <sensor_msgs/Image.h>
-//#include <sensor_msgs/Imu.h>
 #include <sensor_msgs/CameraInfo.h>
-
 #include <ait_ros_messages/VioSensorMsg.h>
+#include <vector>
+
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
 
 #include <ros/ros.h>
-#include <vector>
+
 #include <stdio.h>
 
 namespace duo_avoidance {
@@ -27,10 +33,14 @@ private:
 
   ros::NodeHandle nh_;
   ros::Subscriber duo_message_sub_;
+  ros::Publisher test_pub_;
+  cv::Mat img_l_;
+  cv::Mat img_r_;
 
 //  ros::Publisher vio_pub_;
 //  std::vector<sensor_msgs::Imu> imu_;
   ait_ros_messages::VioSensorMsg duo_msg_;
+  sensor_msgs::Image test_output_;
 
   void duoMessageCb(const ait_ros_messages::VioSensorMsg::ConstPtr &msg);
 
--- src/duo_avoidance.cpp
+++ src/duo_avoidance.cpp
@@ -12,13 +12,26 @@ DuoAvoidance::DuoAvoidance() : nh_("~") {
   duo_message_sub_ = nh_.subscribe<ait_ros_messages::VioSensorMsg>(
       "/vio_sensor", 2, &DuoAvoidance::duoMessageCb, this);
 
-  //vio_pub_ = nh_.advertise<ait_ros_messages::VioSensorMsg>("/vio_sensor", 1);
+  test_pub_ = nh_.advertise<sensor_msgs::Image>("/test_left_image", 1);
 }
 
 void DuoAvoidance::duoMessageCb(const ait_ros_messages::VioSensorMsg::ConstPtr &msg) {
 
 ait_ros_messages::VioSensorMsg duo_msg_  = *msg;
-printf("Received message from Duo3d-Cam. Timestamp: %d,%d\n",duo_msg_.header.stamp.sec,duo_msg_.header.stamp.nsec);
+printf("Received message from Duo3d-Cam. Timestamp: %d.%d\n",duo_msg_.header.stamp.sec,duo_msg_.header.stamp.nsec);
+
+//Use cv_bridge to transform images to cv::Mat
+cv::Mat img_l_=cv_bridge::toCvCopy(duo_msg_.left_image,"mono8")->image;
+cv::Mat img_r_=cv_bridge::toCvCopy(duo_msg_.right_image,"mono8")->image;
+
+cv_bridge::CvImage cv_out;
+cv_out.image=img_l_;
+cv_out.encoding="mono8";
+
+cv_out.toImageMsg(test_output_);
+test_pub_.publish(test_output_);
+
+//Republish left image
 
 
   // sensor_msgs::Image left = *msg;
